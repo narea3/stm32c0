72981decd06bc2287c0554f4a6499c0cdf77a06a
narea3 <118152163+narea3@users.noreply.github.com>
2026-02-12 18:25:59 +0900

    Update main.c

--- LED_Practice/02_Button_Toggle/main.c
+++ LED_Practice/02_Button_Toggle/main.c
@@ -19,6 +19,7 @@ int main(void){
     // 1. 버튼이 눌렸는지 확인
     // PULLUP 상태이기 때문에 초기 set(1) 상태
     if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET){
+      while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET);
 
       // 2. 상태를 뒤집기
       led_state = !led_state;
@@ -26,11 +27,9 @@ int main(void){
       // 3. 바뀐 상태를 LED에 적용
       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, (GPIO_PinState)led_state);
 
-      // 4. 버튼을 떼기 전까지 기다려주기
-      while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET);
       
       // 5. 아주 잠깐 쉬어서 전기적 노이즈 방지
-      HAL_Delay(50);
+      HAL_Delay(100);
     }
   }
 }
